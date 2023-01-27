d3c70c48131c5ab6613830a11414734bf7a244ac
Pieter Wigboldus <pieter.wigboldus@pon.com>
2023-01-27 15:20:55 +0100

    Reduce duplicate code

--- src/parse-filter.test.js
+++ src/parse-filter.test.js
@@ -2,24 +2,26 @@ import test from 'node:test';
 import assert from 'node:assert';
 import { parseFilters } from './parse-filters.js';
 
+const filters = [
+    {
+        default: 0,
+        name: 'page',
+        type: 'number',
+    },
+    {
+        default: 42,
+        name: 'size',
+        type: 'number',
+    },
+];
+
 test('Parse filters use case', async (t) => {
     await t.test('Test if it parse the filters with param', async () => {
         const params = {
             page: 42,
             size: 12,
         };
-        const filters = [
-            {
-                default: 0,
-                name: 'page',
-                type: 'number',
-            },
-            {
-                default: 42,
-                name: 'size',
-                type: 'number',
-            },
-        ];
+
         const result = parseFilters(params, filters);
         const expectedResult = {
             page: 42,
@@ -31,18 +33,7 @@ test('Parse filters use case', async (t) => {
 
     await t.test('Test if it receive the default values', async () => {
         const params = {};
-        const filters = [
-            {
-                default: 0,
-                name: 'page',
-                type: 'number',
-            },
-            {
-                default: 42,
-                name: 'size',
-                type: 'number',
-            },
-        ];
+
         const result = parseFilters(params, filters);
         const expectedResult = {
             page: 0,
